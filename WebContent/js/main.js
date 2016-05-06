5fa77857a9bc69645395900f306282ea86e3629c
王真真 <1416800961@qq.com>
2016-05-06 22:32:21 +0800

    网盘部分一
    图片，视频，音乐，回收站

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -66,5 +66,94 @@ jQuery(function($) {'use strict',
 	//Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
+
+	$('.img_box').mouseover(function(){
+		$(this).addClass("moveover_img_box");
+		$(this).find(".img_checkbox").css("display","block");
+		$(this).find(".playIcon").css("display","block");
+    }).mouseout( function(){
+        $(this).removeClass("moveover_img_box");
+		if($(this).hasClass("item_active")){
+			//Do nothing.
+		}else{
+			$(this).find(".img_checkbox").hide();
+		}
+		$(this).find(".playIcon").css("display","none");
+     });
+	 
+	$('.img_checkbox').click(function() {
+		$(this).css({"display":"block","background-position":"-30px -36px"});
+		$(this).parent().addClass("item_active");
+	});
+	
+/*	$('.img_checkbox').toggle(function(){
+        $(this).css({"display":"block","background-position":"-30px -36px"});
+		$(this).parent().addClass("item_active");
+		alert("添加啊");
+    },function(){
+		$(this).css("display","none");
+		$(this).parent().removeClass("item_active");
+		alert("添加啊试试");
+    });*/
+	
+	$('.sharebutton').click(function() {
+		$("#share").css("display","block");
+	});
+	
+	$('.share-close').click(function() {
+		$("#share").css("display","none");
+	});
+	
+	$('.deletebutton').click(function() {
+		$("#confirm").css("display","block");
+	});
+	
+	$('.confirm-close').click(function() {
+		$("#confirm").css("display","none");
+	});
+	/*music_all.html*/
+	$('.my_checkbox').click(function() {
+		$(this).css({"display":"block","background-position":"-31px 12px"});
+		$(this).parent().addClass("item_active");
+	});
+	
+	$('.list-view-item').mouseover(function(){
+		$(this).addClass("hover-item");
+		$(this).find(".operate").css("display","block");
+    }).mouseout( function(){
+        $(this).removeClass("hover-item");
+		$(this).find(".operate").css("display","none");
+    });
+	/*video_all.html*/
+	$('.list-switch').click(function() {
+		$(this).css("background-position","0 0");
+		$(this).siblings(".grid-switch").css("background-position","right 0");
+		$(".grid-view-container").css("display","none");
+		$(".list-view-container").css("display","block");
+	});
+	$('.grid-switch').click(function() {
+		$(this).css("background-position","right -35px");
+		$(this).siblings(".list-switch").css("background-position","0 -35px");
+		$(".grid-view-container").css("display","block");
+		$(".list-view-container").css("display","none");
+	});
+	/*recycle_bin.html*/
+	$('.restore_button').click(function() {
+		$("#restore_conform").css("display","block");
+	});
+	
+	$('.restore_confirm_close').click(function() {
+		$("#restore_conform").css("display","none");
+	});
+	
+	$('.final_deletebutton').click(function() {
+		$("#final_delete_confirm").css("display","block");
+	});
+	
+	$('.final_delete_confirm_close').click(function() {
+		$("#final_delete_confirm").css("display","none");
+	});
+	
+
 });
